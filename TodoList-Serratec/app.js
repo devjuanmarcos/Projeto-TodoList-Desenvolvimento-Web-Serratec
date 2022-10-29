22c8fe1a26a46a54117d9e3074e3e054a850009e
JottaEnnie <jncontatojn@gmail.com>
2022-10-28 23:55:57 -0300

    salvando dados em localStorage

--- TodoList-Serratec/app.js
+++ TodoList-Serratec/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-let banco = [
-    { 'tarefa': 'Estudar JS', 'status': '' },
-    { 'tarefa': 'netflix', 'status': 'checked' },
-    { 'tarefa': 'teste', 'status': 'checked' }
-];
+let banco = [];
+
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
 
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
@@ -23,8 +22,10 @@ const limparTarefas = () => {
         todoList.removeChild(todoList.lastChild);
     }
 }
+
 const atualizarTela = () => {
     limparTarefas();
+    const banco = getBanco();
     banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 }
 
@@ -32,30 +33,37 @@ const inserirItem = (evento) => {
     const tecla = evento.key;
     const texto = evento.target.value;
     if (tecla === 'Enter') {
-        banco.push({ 'tarefa': texto, 'status': 'checked' })
+        const banco = getBanco();
+        banco.push({ 'tarefa': texto, 'status': '' })
+        setBanco(banco);
         atualizarTela();
         evento.target.value = '';
     }
 }
 
-const removerItem= (indice) => {
-    banco.splice (indice,1);
+const removerItem = (indice) => {
+    const banco = getBanco();
+    banco.splice(indice, 1);
+    setBanco(banco);
     atualizarTela();
 }
 
 const atualizarItem = (indice) => {
+    const banco = getBanco();
     banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
     atualizarTela();
 }
 
 const clickItem = (evento) => {
     const elemento = evento.target;
-    if(elemento.type === 'button') {
+    console.log (elemento.type);
+    if (elemento.type === 'button') {
         const indice = elemento.dataset.indice;
         removerItem(indice);
-    }else if (elemento.type === 'checkbox') {
+    } else if (elemento.type === 'checkbox') {
         const indice = elemento.dataset.indice;
-        atualizarItem (indice);
+        atualizarItem(indice);
     }
 }
 
