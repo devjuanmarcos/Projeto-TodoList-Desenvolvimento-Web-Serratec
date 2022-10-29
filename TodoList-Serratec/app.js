dd3a76549fe06cecef9fc2c906f87dd9ff410f6d
JottaEnnie <jncontatojn@gmail.com>
2022-10-28 23:16:15 -0300

    indentificando as tarefas com indice

--- TodoList-Serratec/app.js
+++ TodoList-Serratec/app.js
@@ -6,13 +6,13 @@ let banco = [
     { 'tarefa': 'teste', 'status': 'checked' }
 ];
 
-const criarItem = (tarefa, status) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox" ${status}>
+    <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
-    <input type="button" value="X">
+    <input type="button" value="X" data-indice=${indice}>
     `;
     document.getElementById('todoList').appendChild(item);
 }
@@ -25,19 +25,25 @@ const limparTarefas = () => {
 }
 const atualizarTela = () => {
     limparTarefas();
-    banco.forEach(item => criarItem(item.tarefa, item.status));
+    banco.forEach( (item, indice) => criarItem(item.tarefa, item.status, indice));
 }
 
 const inserirItem = (evento) => {
     const tecla = evento.key;
-    const texto = evento.target.value; 
-    if (tecla === 'Enter'){
-        banco.push ({ 'tarefa': texto, 'status': 'checked' })
+    const texto = evento.target.value;
+    if (tecla === 'Enter') {
+        banco.push({ 'tarefa': texto, 'status': 'checked' })
         atualizarTela();
         evento.target.value = '';
     }
-} 
+}
 
-document.getElementById('newItem').addEventListener('keypress',inserirItem);
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log(elemento);
+}
 
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
 atualizarTela();
+
