0986a636edc8585a46a5ca3355cc8162a72905b7
JottaEnnie <jncontatojn@gmail.com>
2022-10-28 22:24:53 -0300

    criando funcao de atualizacao de tela

--- TodoList-Serratec/app.js
+++ TodoList-Serratec/app.js
@@ -1,14 +1,31 @@
 'use strict';
 
-const criarItem = (tarefa) => {
+let banco = [
+    { 'tarefa': 'Estudar JS', 'status': '' },
+    { 'tarefa': 'netflix', 'status': 'checked' },
+    { 'tarefa': 'teste', 'status': 'checked' }
+];
 
+const criarItem = (tarefa, status) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox">
+    <input type="checkbox" ${status}>
     <div>${tarefa}</div>
     <input type="button" value="X">
-    `
+    `;
     document.getElementById('todoList').appendChild(item);
-
 }
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+}
+
+atualizarTela();
