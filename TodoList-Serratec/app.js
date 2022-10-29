da5d6e1f8a21063dce4fd57a32da9671d73048df
JottaEnnie <jncontatojn@gmail.com>
2022-10-28 21:59:07 -0300

    alterações básicas

--- TodoList-Serratec/app.js
+++ TodoList-Serratec/app.js
@@ -1,4 +1,14 @@
-const criarItem = () {
+'use strict';
 
-    
+const criarItem = (tarefa) => {
+
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+    <input type="checkbox">
+    <div>${tarefa}</div>
+    <input type="button" value="X">
+    `
+    document.getElementById('todoList').appendChild(item);
+
 }
