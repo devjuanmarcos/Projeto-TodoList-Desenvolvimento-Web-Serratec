5a19677a82a93539f5130d30631d5b678210701f
JottaEnnie <jncontatojn@gmail.com>
2022-10-30 16:56:19 -0300

    botao e caixa de selecao feitos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Form from './components/Form';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <TodoItem />
     </div>
   );
 }
--- todo/src/components/Form.jsx
+++ todo/src/components/Form.jsx
@@ -1,10 +1,11 @@
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import React from 'react';
 
 export default function Form(){
     return(
         <div>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button variant="text">Adicionar</Button>
         </div>
     );
 }
