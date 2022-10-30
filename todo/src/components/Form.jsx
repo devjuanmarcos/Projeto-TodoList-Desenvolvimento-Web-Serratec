f62943d6f865b8b40b190b58758755dd04d75051
JottaEnnie <jncontatojn@gmail.com>
2022-10-30 20:05:03 -0300

    remover texto ao adicionar tarefa funcionando

--- todo/src/components/Form.jsx
+++ todo/src/components/Form.jsx
@@ -1,14 +1,15 @@
 import { Button, Paper, TextField } from '@mui/material';
 import React, { useState } from "react";
 
-export default function Form({addTodo}) {
+export default function Form({ addTodo }) {
     const [text, setText] = useState(null);
     const [id, setId] = useState(0);
 
     const todoCreate = (text) => {
-        const todoObj = {text: text, id: id};
-        setId(id +1);
+        const todoObj = { text: text, id: id };
+        setId(id + 1);
         addTodo(todoObj);
+        document.getElementById("outlined-basic").value = null;
     };
 
     return (
